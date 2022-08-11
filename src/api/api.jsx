cc371a70c15a3cf57ead30b3b2aad8304a6fe6da
AlekseyDemydov <96746949+AlekseyDemydov@users.noreply.github.com>
2022-08-11 19:22:31 +0300

    Update api.jsx

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -3,11 +3,10 @@ import axios from 'axios';
 class apiFetch {
   #BASE_URL = 'https://api.themoviedb.org/3/';
   #API_KEY = 'b2701829e6cba5c18fb7b3c9d2adde66';
-  #SEARCH = 'search/movie';
+
   #TRAND = 'trending/movie/day';
   #ALL = 'movie/';
 
-  /* Запрос на ТРЕНДЫ */
   trend = async page => {
     const respons = await axios.get(
       `${this.#BASE_URL}${this.#TRAND}?api_key=${this.#API_KEY}&page=${page}`
@@ -15,17 +14,6 @@ class apiFetch {
     return respons.data.results;
   };
 
-  /* Запрос ПОИСК ИНПУТ */
-  search = async (query, page) => {
-    const respons = await axios.get(
-      `${this.#BASE_URL}${this.#SEARCH}?api_key=${
-        this.#API_KEY
-      }&query=${query}&page=${page}`
-    );
-    return respons.data;
-  };
-
-  /* Запрос id */
   id = async id => {
     const respons = await axios.get(
       `${this.#BASE_URL}${this.#ALL}/${id}?api_key=${this.#API_KEY}`
@@ -33,7 +21,6 @@ class apiFetch {
     return respons.data;
   };
 
-  /* Запрос Cast */
   cast = async id => {
     const respons = await axios.get(
       `${this.#BASE_URL}${this.#ALL}/${id}/credits?api_key=${this.#API_KEY}`
@@ -41,7 +28,6 @@ class apiFetch {
     return respons.data.cast;
   };
 
-  /* Запрос Reviews */
   reviews = async id => {
     const respons = await axios.get(
       `${this.#BASE_URL}${this.#ALL}/${id}/reviews?api_key=${this.#API_KEY}`
